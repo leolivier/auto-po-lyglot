5bbd57ea54b7cf617e88d8ec28e3c1564d1349bd
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-22 18:42:45 +0200

    added a cached implementation based on beta implementation from Claude (not yest tested)

--- transpo_claude.py
+++ transpo_claude.py
@@ -1,6 +1,6 @@
 import anthropic
 
-from transpo_abc import TranspoClient, TranspoException
+from transpo_base import TranspoClient, TranspoException
 
 
 class ClaudeClient(TranspoClient):
@@ -13,7 +13,7 @@ class ClaudeClient(TranspoClient):
       message = self.client.messages.create(
         model=self.model,
         max_tokens=1000,
-        temperature=0,
+        temperature=0.2,
         system=system_prompt,
         messages=[
             {
@@ -30,3 +30,52 @@ class ClaudeClient(TranspoClient):
       return message.content
     except Exception as e:
       raise TranspoException(str(e))
+
+
+class CachedClaudeClient(ClaudeClient):
+
+  def __init__(self, original_language, context_language, target_language, api_key, model=None):
+    super().__init__(original_language, context_language, target_language, api_key, model)
+    self.cached_system_prompt = None
+
+  def get_system_prompt(self):
+    """
+    Returns the system prompt for the translation task, caching the result to avoid repeated computation.
+
+    Parameters:
+    None
+
+    Returns:
+    str: The system prompt for the translation task, or None if the prompt has not changed since the last call.
+    """
+    super_prompt = super().get_system_prompt()
+    if self.cached_system_prompt is None:
+      self.cached_system_prompt = super_prompt
+      return super_prompt
+    elif self.cached_system_prompt != super_prompt:
+      self.cached_system_prompt = super_prompt
+    return None
+
+  def get_translation(self, system_prompt, user_prompt):
+    # TODO: This works only for ttl of the cache ==> can be improved
+    if system_prompt is not None:  # Means the system prompt has changed since last call and must be cached
+      try:
+        # uses a beta endpoint, changes in the future
+        response = self.client.beta.prompt_caching.messages.create(
+          model=self.model,
+          max_tokens=1024,
+          temperature=0.2,
+          system=[
+            {
+              "type": "text",
+              "text": system_prompt,
+              "cache_control": {"type": "ephemeral"}
+            }
+          ],
+          messages=[{"role": "user", "content": user_prompt}],
+        )
+        return response.content
+      except Exception as e:
+        raise TranspoException(str(e))
+    else:  # system prompt has not changed
+      return super().get_translation(system_prompt, user_prompt)
