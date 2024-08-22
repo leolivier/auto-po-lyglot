b29ecc66241384329ab5ddd48c20b590dc0489e0
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-22 18:38:43 +0200

    moved to tries as prompt management has been refactored

--- tries/transpo_prompts.py
+++ tries/transpo_prompts.py
@@ -1,4 +1,4 @@
-from transpo_abc import TranspoPrompts
+from transpo_base import TranspoPrompts
 
 
 class TranspoPromptsImpl1(TranspoPrompts):
@@ -13,9 +13,9 @@ for this {self.original_language} sentence that you will consider for desambigua
 Please respond with the best translation you find for the {self.context_language} sentence. If you need to provide
 an explanation of the translation, please do so but only after giving the best translation and on another line."""
 
-  def get_user_prompt(self, phrase, contextual_translation):
+  def get_user_prompt(self, phrase, context_translation):
     return (f"""{self.original_language} sentence: "{phrase}", {self.context_language} translation:"""
-            f""" "{contextual_translation}", {self.target_language}:""")
+            f""" "{context_translation}", {self.target_language}:""")
 
 
 class TranspoPromptsImpl2(TranspoPrompts):
@@ -23,11 +23,11 @@ class TranspoPromptsImpl2(TranspoPrompts):
   def get_system_prompt(self):
     return "You are a helpful assistant that translates text."
 
-  def get_user_prompt(self, phrase, contextual_translation):
+  def get_user_prompt(self, phrase, context_translation):
     return (
       f"Translate the following {self.original_language} sentence into {self.target_language},"
       f"considering the provided {self.context_language} context for disambiguation:\n"
       f"{self.original_language}: '{phrase}'\n"
-      f"{self.context_language} context: '{contextual_translation}'\n"
+      f"{self.context_language} context: '{context_translation}'\n"
       f"Provide only the {self.target_language} translation."
     )
