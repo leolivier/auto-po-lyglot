78510013817854fa74fc4c1ecd21a4b3196960f4
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-24 16:08:14 +0200

    refactored args parsing, llm client and .env management

---
+++ getenv.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import sys
+from dotenv import load_dotenv
+from os import environ
+import json
+import argparse
+
+
+class TranspoParams:
+  description = """
+Generates a translation file using a given model and llm type. It reads the parameters from the command line,
+and completes them when necessary from the content of .env in the same directory.
+It iterates over a list of test translations containing the original phrase and its translation
+within a context language, and for each target language, translates the original phrase
+into the target language helped with the context translation, by using the provided client and
+prompt implementation."""
+
+  def parse_args(self, additional_args=None):
+    parser = argparse.ArgumentParser(description=self.description)
+    # Add arguments
+    parser.add_argument('--llm',
+                        type=str,
+                        help='Le type of LLM you want to use. Can be openai, ollama, claude or claude_cached. '
+                             'For openai or claude[_cached], you need to set the api key in the environment')
+    parser.add_argument('--model',
+                        type=str,
+                        help='the name of the model to use. If not provided, a default model '
+                             'will be used, based on the chosen client')
+    parser.add_argument('--original_language',
+                        type=str,
+                        help='the language of the original phrase')
+    parser.add_argument('--context_language',
+                        type=str,
+                        help='the language of the context translation')
+    parser.add_argument('--target_language',
+                        type=str,
+                        help='the language into which the original phrase will be translated')
+    parser.add_argument('--verbose', action='store_true', help='verbose mode')
+    for arg in additional_args:
+      parser.add_argument(arg.get('arg'), type=arg.get('type'), help=arg.get('help'))
+
+    # Analyze the arguments
+    return parser.parse_args()
+
+  def __init__(self, additional_args=None):
+    "looks at args and returns an object with attributes of these args completed by the environ variables where needed"
+
+    args = self.parse_args(additional_args)
+
+    load_dotenv(override=True)
+
+    # where tests results will be stored
+    self.output_dir = args.output_dir or environ.get('OUTPUT_DIRECTORY', '.')
+    # .po file context:
+    # primary language (msgids)
+    self.original_language = args.original_language or environ.get('ORIGINAL_LANGUAGE', 'English')
+    # and translation language (msgstrs)
+    self.context_language = args.context_language or environ.get('CONTEXT_LANGUAGE', 'French')
+    # chose the LLM client and model
+    self.llm_client = args.llm or environ.get('LLM_CLIENT', 'ollama')
+    self.model = args.model or environ.get('LLM_MODEL', None)
+
+    # the target languages to test for translation
+    if args.target_language:
+      self.test_target_languages = [args.target_language]
+    else:
+      self.test_target_languages = environ.get('TARGET_LANGUAGES', 'Spanish').split(',')
+
+    # some ambiguous original sentences and their context translations for testing
+    if args.original_phrase:
+      if not args.context_translation:
+        print("context_translation must be set when origin_phrase is set")
+        sys.exit(1)
+
+      self.translations_testset = [{"original_phrase": args.original_phrase,
+                                    "context_translation": args.context_translation}]
+    else:
+      TEST_TRANSLATIONS = environ.get(
+        'TEST_TRANSLATIONS',
+        """[{"original_phrase": "He gave her a ring.", "context_translation": "Il lui a donné une bague."}]""")
+      try:
+        self.translations_testset = json.loads(TEST_TRANSLATIONS)
+      except json.decoder.JSONDecodeError:
+        print("TEST_TRANSLATIONS must be a valid JSON array\n", TEST_TRANSLATIONS)
+        sys.exit(1)
+    # print(self.translations_testset)
+
+  def get_client(self):
+    match self.llm_client:
+      case 'ollama':
+        from openai_ollama_client import OllamaClient as LLMClient
+      case 'openai':
+        # uses OpenAI GPT-4o by default
+        from openai_ollama_client import OpenAIClient as LLMClient
+      case 'claude':
+        # uses Claude Sonnet 3.5 by default
+        from claude_client import ClaudeClient as LLMClient
+      case 'claude_cached':
+        # uses Claude Sonnet 3.5, cached mode for long system prompts
+        from claude_client import CachedClaudeClient as LLMClient
+      case _:
+        raise Exception(
+          f"LLM_CLIENT must be one of 'ollama', 'openai', 'claude' or 'claude_cached', not '{self.llm_client}'"
+          )
+    return LLMClient(self.original_language, self.context_language, "", model=self.model) if self.model \
+      else LLMClient(self.original_language, self.context_language, "")
