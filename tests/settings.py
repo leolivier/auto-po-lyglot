44a85c6d8a4b09ac475537556db1d4296f86288c
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-29 20:17:16 +0200

    restrict the number of tests on Github Actions to 3

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,31 @@
 # Where tests results will be stored. Can be overriden on the command line
+from os import environ
+
+
 OUTPUT_DIRECTORY = "./tests/output"
 
 # Some ambiguous sentences in the ORIGINAL_LANGUAGE and their CONTEXT_LANGUAGE translations for testing
 TEST_TRANSLATIONS = [
+      {
+        "original_phrase": "He gave her a ring.",
+        "context_translation": "Il lui a donné une bague.",
+        "target_translation": "Lui ha regalato un anello."
+      },
+      {
+        "original_phrase": "She made a call.",
+        "context_translation": "Elle a pris une décision.",
+        "target_translation": "Lei ha preso una decisione."
+      },
+      {
+        "original_phrase": "They left the room.",
+        "context_translation": "Ils ont quitté la pièce.",
+        "target_translation": "Si sono andati dalla stanza."
+      },
+      {
+        "original_phrase": "He gave her a ring.",
+        "context_translation": "Il lui a passé un coup de fil.",
+        "target_translation": "Lui ha regalato un anello."
+      },
       {
         "original_phrase": "She broke down",
         "context_translation": "Elle est tombée en panne",
@@ -39,28 +62,12 @@ TEST_TRANSLATIONS = [
         "target_translation": "Lei ha fatto una chiamata."
       },
       {
-        "original_phrase": "She made a call.",
-        "context_translation": "Elle a pris une décision.",
-        "target_translation": "Lei ha preso una decisione."
-      },
-      {
-        "original_phrase": "They left the room.",
-        "context_translation": "Ils ont quitté la pièce.",
-        "target_translation": "Si sono andati dalla stanza."
-      },
-      {
         "original_phrase": "They left the room.",
         "context_translation": "Ils ont laissé la pièce en l'état.",
         "target_translation": "Si hanno lasciato la stanza."
       },
-      {
-        "original_phrase": "He gave her a ring.",
-        "context_translation": "Il lui a donné une bague.",
-        "target_translation": "Lui ha regalato un anello."
-      },
-      {
-        "original_phrase": "He gave her a ring.",
-        "context_translation": "Il lui a passé un coup de fil.",
-        "target_translation": "Lui ha regalato un anello."
-      }
     ]
+
+# use only 3 first translations for github actions
+if environ.get("GITHUB_ACTIONS") == "true":
+  TEST_TRANSLATIONS = TEST_TRANSLATIONS[:3]
