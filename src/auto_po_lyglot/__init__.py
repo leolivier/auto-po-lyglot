d0054e139d7877f803f54368f2e4f887902616f5
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-09-07 19:19:09 +0200

    typo

--- src/auto_po_lyglot/__init__.py
+++ src/auto_po_lyglot/__init__.py
@@ -2,7 +2,7 @@ from .getenv import ParamsLoader
 from .csv_extractor import extract_csv
 from .openai_ollama_client import OpenAIAPICompatibleClient, OpenAIClient, OllamaClient
 from .claude_client import ClaudeClient, CachedClaudeClient
-from base import TranspoClient
+from .base import TranspoClient
 
 __all__ = [
   'ParamsLoader',
