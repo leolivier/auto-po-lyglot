34f1b59da7697e3fdaf13bba76b279a36fb564c7
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-09-07 21:14:06 +0200

    fixed get prompt

--- src/auto_po_lyglot/base.py
+++ src/auto_po_lyglot/base.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import logging
-from os import environ
 from .default_prompts import (
   system_prompt as default_system_prompt,
   user_prompt as default_user_prompt,
@@ -41,8 +40,9 @@ class TranspoClient(ABC):
     ...
 
   def get_system_prompt(self):
-    format = self.params.system_prompt if hasattr(self.params, 'system_prompt') else default_system_prompt
+    format = self.params.system_prompt or default_system_prompt
     logger.debug("system prompt format: ", format)
+    # print("default system prompt format: ", default_system_prompt)
     try:
       basic_exemple = basic_examples[0]
       assert self.params.original_language in basic_exemple
@@ -109,7 +109,7 @@ class TranspoClient(ABC):
     return system_prompt
 
   def get_user_prompt(self, phrase, context_translation):
-    format = environ.get("USER_PROMPT", None) or default_user_prompt
+    format = self.params.user_prompt or default_user_prompt
     if format is None:
       raise TranspoException("USER_PROMPT environment variable not set")
     params = {
